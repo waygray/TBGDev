34f12ea8669cf67d5b973ef4fb31fa06b2a0036e
waygray <waylon.95.race@gmail.com>
2025-09-02 13:13:11 -0600

    changed string and give info func

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ def get_player_name():
     return print("Hello, " + user_input + ", Welcome to game!")
 
 def give_info():
-    return print("Something is going to go here, the hope to to teach myself more indepth python with a long TBSG")
+    return print(
+        """
+        Something is going to go here, I dont know what I want to make this game about
+         yet, this is mainly something so I have something fun to code, but to help myself learn, 
+         python on a deeper level. Thanks for Reading!(If you see this before I change this, Why are you here?)
+        """
+    )
 
 
 
