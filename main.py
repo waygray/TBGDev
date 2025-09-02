b4dcaa3bb2c174500e3fd45f7514eb27378f5611
waygray <waylon.95.race@gmail.com>
2025-09-02 13:09:15 -0600

    added 2 funcs to get started

---
+++ main.py
@@ -0,0 +1,15 @@
+def get_player_name():
+    user_input = input("Enter player name: ")
+    return print("Hello, " + user_input + ", Welcome to game!")
+
+def give_info():
+    return print("Something is going to go here, the hope to to teach myself more indepth python with a long TBSG")
+
+
+
+def main():
+    get_player_name()
+    give_info()
+
+if __name__ == '__main__':
+    main()
